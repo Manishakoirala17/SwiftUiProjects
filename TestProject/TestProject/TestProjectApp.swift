5a85843e9e47021a1d59967169209c2e1efd6537
vignesh <vignesh@dreamtusk.com>
2023-07-04 21:32:31 +0530

    create new project

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Manisha on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
